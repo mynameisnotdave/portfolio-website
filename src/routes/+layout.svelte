9f837b7ae7683b2aca3582d755f60fc04f07fbe7
mynameisnotdave <islandmonkeee@gmail.com>
2023-11-05 15:27:22 +0000

    layout stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang='ts'>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell slotPageHeader="w-screen border-2 border-solid">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Aidan Fell</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm"
+					href="https://github.com/mynameisnotdave"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
