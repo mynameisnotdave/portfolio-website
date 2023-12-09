7cdba49c9fe12eb70407047769f1bfffedcef472
mynameisnotdave <islandmonkeee@gmail.com>
2023-12-09 09:33:37 +0000

    get this up and working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html class="font-sans text-base" lang="en">
+    <head>
+        <title>Aidan Fell</title>
+    </head>
+    <body>
+        <header><h1 class="uppercase">Hello</h1></header>
+        <div class="border-2 rounded-none border-solid outline-2
+ outline-blue-500 outline-offset-0 shadow-md bg-red-700">
+            <p>Hi, I'm Aidan and I'm a .NET software developer from the south of England.</p>
+        </div>
+    </body>
+</html>
